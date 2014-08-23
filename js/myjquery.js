bc64688c01944da19065896c8fdcd59c9b48b718
Sypruskung <namsyprus.paisley@gmail.com>
2014-08-24 04:01:05 +0700

    keydown function added for several keys

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	updatecurrent();
+
 });
 
 $("#navbar li a").click(function(e) {
@@ -8,93 +10,85 @@ $("#navbar li a").click(function(e) {
      
     var goto = $(this).attr('href');
  
-    $('html, body').animate({
-        scrollTop: $(goto).offset().top
-    }, 500);
+    gotoelement(goto);
 
-    var $prevLi = $('#navbar ul li.active');
-    $(this).parent('li').addClass('active');
-    $prevLi.removeClass('active');
+    updatecurrent();
 
 });
 
-$('.up-button').click(function() {
+// $('.up-button').click(function() {
+
+// 	pageup();
+
+// });
+
+// $('.down-button').click(function() {
+
+// 	pagedown();
+
+// });
+
+$(document).keydown(function (e) {
+
+	if (e.keyCode == 33 || e.keyCode == 38) { //pageup, arrowup
+		pageup();
+	} 
+
+	if (e.keyCode == 34 || e.keyCode == 40) { //pagedown, arrowdown
+		pagedown();
+	}
+
+	if (e.keyCode == 36) { //home
+		gotoelement('#aboutme');
+	}
+
+	if (e.keycode == 35) { //end
+		gotoelement('#friends');
+	}
+
+});
+
+$(window).scroll(function(){ // bind window scroll event
+
+	updatecurrent();
+
+});
+
+var updatecurrent = function() {
+
+	var id = $('.contentbox:onScreen').attr('id');
+	var $oldLi = $('#navbar ul li.active');
+	var $newLi = $("[href='#" + id + "']").parent('li');
+
+	$oldLi.removeClass('active');
+	$newLi.addClass('active');
+
+};
+
+var pageup = function() {
 
 	var $thisLi = $('#navbar ul li.active');
 	var prev = $thisLi.prev().find('a').attr('href');
 	
-	$('html, body').animate({
-		scrollTop: $(prev).offset().top
-	}, 500);
+	gotoelement(prev);
 
-    $thisLi.prev().addClass('active');
-    $thisLi.removeClass('active');
+}
 
-});
-
-$('.down-button').click(function() {
+var pagedown = function() {
 
 	var $thisLi = $('#navbar ul li.active');
 	var next = $thisLi.next().find('a').attr('href');
 
+	gotoelement(next);
+
+}
+
+var gotoelement = function(goto) {
+
 	$('html, body').animate({
-		scrollTop: $(next).offset().top
-	}, 500);
+        scrollTop: $(goto).offset().top
+    }, 500);
 
-	$thisLi.next().addClass('active');
-    $thisLi.removeClass('active');
+    updatecurrent();
 
-});
-
-// if ($('#aboutme').visible(false)) {
-
-// 	var $prevLi = $('#navbar ul li.active');
-// 	var $gotoLi = $("[href='#aboutme']").parent('li');
-
-// 	$gotoLi.addClass('active');
-// 	$prevLi.removeClass('active');
-
-// } else {
-//     // The element is NOT visible, do something else
-// }
-
-$.fn.is_on_screen = function(){
-    var win = $(window);
-    var viewport = {
-        top : win.scrollTop(),
-        left : win.scrollLeft()
-    };
-    viewport.right = viewport.left + win.width();
-    viewport.bottom = viewport.top + win.height();
- 
-    var bounds = this.offset();
-    bounds.right = bounds.left + this.outerWidth();
-    bounds.bottom = bounds.top + this.outerHeight();
- 
-    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
-};
-
-var check_if_current_ele = function(element, button) {
-
-	if($element.is_on_screen()) {
-		var $prevLi = $('#navbar ul li.active');
-    	$button.addClass('active');
-    	$prevLi.removeClass('active');
-	}
-
-};
-
-var update_current_ele = function() {
-	check_if_current_ele($('#friendlist'), $("[href='#friends']").parent('li'));
-};
-
-// if( $('.target').length > 0 ) { // if target element exists in DOM
-// 	if( $('.target').is_on_screen() ) { // if target element is visible on screen after DOM loaded
-//         $('.log').html('<div class="alert alert-success">target element is visible on screen</div>'); // log info		
-// 	} else {
-//         $('.log').html('<div class="alert">target element is not visible on screen</div>'); // log info
-// 	}
-// }
-$(window).scroll(function(){ // bind window scroll event
-	update_current_ele();
-});
+}
