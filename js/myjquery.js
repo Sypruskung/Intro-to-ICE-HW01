6361303dd005eb11f57cd37c7e69af5d1c86d355
Sypruskung <namsyprus.paisley@gmail.com>
2014-08-29 23:32:21 +0700

    google maps added

--- js/myjquery.js
+++ js/myjquery.js
@@ -4,6 +4,27 @@ $(document).ready(function(){
 
 });
 
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+	var mapProp = {
+		center:new google.maps.LatLng(13.7383,100.5324),
+		zoom:15,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		title: 'Hello World!'
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 $("#navbar li a").click(function(e) {
 
 	e.preventDefault();
